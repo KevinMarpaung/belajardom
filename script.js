b54982f058269e13e05efdc9dec97e1caaa8f61f
KevinMarpaung <kmarpaung430@gmail.com>
2023-09-04 20:08:45 +0700

    belajar dom

---
+++ script.js
@@ -0,0 +1,8 @@
+document.writeln("<p>saya sedang belajar dom </p>");
+document.writeln("<h2>semoga harimu menyenangkan</h2>");
+
+let judul = document.getElementById("judul");
+judul.style.backgroundColor = "gold";
+judul.style.textAlign = "center";
+judul.style.borderRadius = "4px";
+judul.style.padding = "4px";
